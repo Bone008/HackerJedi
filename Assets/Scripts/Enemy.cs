0cf9da9a9638e1d978f523cd3bf8ef3dea878bab
Peter Greth <peter.greth@gmx.de>
2017-04-04 18:38:16 +0200

    Added moritz' nav mesh work to move the stupid enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,29 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
-
-    public Transform target;
+public class Enemy : MonoBehaviour
+{
+    public float newTargetPosThreshhold, stopRange;
+    public Transform goal;
     public float hitRange;
-    public float movementSpeed;
 
+    private Vector3 oldPos;
+    private NavMeshAgent agent;
+    private bool following = true;
     private Gun gun;
 
-	void Start () {
+
+    void Start()
+    {
+        oldPos = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+
         // get gun component from children
         gun = GetComponentInChildren<Gun>();
-	}
-	
-	void Update () {
-        // move towards and look to target
-        transform.position = Vector3.MoveTowards(transform.position, target.position, movementSpeed);
-        transform.LookAt(target);
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(transform.position, goal.transform.position) > stopRange)
+        {
+            if (Vector3.Distance(oldPos, goal.position) > newTargetPosThreshhold)
+            {
+                agent.destination = goal.position;
+                oldPos = goal.position;
+            }
+            following = true;
+        }
+        else
+        {
+            if (following)
+            {
+                Debug.Log("telling him to stop");
+                Debug.Log(Vector3.Distance(transform.position, goal.transform.position));
+                agent.destination = transform.position;
+                following = false;
+            }
+        }
 
         // fire if in range
-		if((target.position - transform.position).magnitude <= hitRange)
+        if ((goal.position - transform.position).magnitude <= hitRange)
         {
             gun.Fire();
         }
-	}
+    }
 }
