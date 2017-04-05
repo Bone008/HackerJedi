c9b9f5453f6719f2e4cd973c8b7b2a67e0a49083
Far Rechner <far-rechner@uni>
2017-04-05 17:50:43 +0200

    completed framework for ability selection with GUI #5
    ... GUI still needs to be prettier

---
+++ Assets/Scripts/AbilitySelectionTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitySelectionTrigger : MonoBehaviour {
+
+    public AbilitySelectionWheel wheel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter", other);
+        var element = other.GetComponent<AbilitySelectionElement>() ?? other.GetComponentInParent<AbilitySelectionElement>();
+        if(element != null)
+            wheel.SelectedElement = element;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var element = other.GetComponent<AbilitySelectionElement>() ?? other.GetComponentInParent<AbilitySelectionElement>();
+        if(wheel.SelectedElement == element)
+            wheel.SelectedElement = null;
+    }
+}
