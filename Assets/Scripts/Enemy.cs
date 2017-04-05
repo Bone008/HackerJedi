2b675da191e26960e8c8d8ca641a83deece3d61f
jungo <ga45jas@mytum.de>
2017-04-05 15:02:32 +0200

    Throw Function in Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,76 +3,104 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+namespace Valve.VR.InteractionSystem
 {
-    public float newTargetPosThreshhold, stopRange;
-    public float hitRange;
-    public float fireDelay = 0.6f;
+    //for throwing the enemy
+    [RequireComponent(typeof(VelocityEstimator))]
+    [RequireComponent(typeof(Rigidbody))]
+    //end
 
-    private Transform goal;
-    private Vector3 oldPos;
-    private NavMeshAgent agent;
-    private bool following = true;
-    private Gun gun;
-
-    private Coroutine firingCoroutine = null;
-
-
-    void Start()
+    public class Enemy : MonoBehaviour
     {
-        // locate player
-        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        public float newTargetPosThreshhold, stopRange;
+        public float hitRange;
+        public float fireDelay = 0.6f;
 
-        oldPos = goal.position;
-        agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        private Transform goal;
+        private Vector3 oldPos;
+        private NavMeshAgent agent;
+        private bool following = true;
+        private Gun gun;
 
-        // get gun component from children
-        gun = GetComponentInChildren<Gun>();
-    }
+        private Coroutine firingCoroutine = null;
+        private VelocityEstimator velocityEstimator; //for throwing the enemy
 
-    void FixedUpdate()
-    {
-        if (Vector3.Distance(transform.position, goal.transform.position) > stopRange)
+        void Start()
         {
-            if (Vector3.Distance(oldPos, goal.position) > newTargetPosThreshhold)
+            // locate player
+            goal = GameObject.FindGameObjectWithTag("Player").transform;
+
+            oldPos = goal.position;
+            agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+
+            // get gun component from children
+            gun = GetComponentInChildren<Gun>();
+        }
+
+        void Awake()
+        {
+            velocityEstimator = GetComponent<VelocityEstimator>();
+        }
+
+        void FixedUpdate()
+        {
+            if (Vector3.Distance(transform.position, goal.transform.position) > stopRange)
             {
-                agent.destination = goal.position;
-                oldPos = goal.position;
+                if (Vector3.Distance(oldPos, goal.position) > newTargetPosThreshhold)
+                {
+                    agent.destination = goal.position;
+                    oldPos = goal.position;
+                }
+                following = true;
             }
-            following = true;
-        }
-        else
-        {
-            if (following)
+            else
             {
-                Debug.Log("telling him to stop");
-                Debug.Log(Vector3.Distance(transform.position, goal.transform.position));
-                agent.destination = transform.position;
-                following = false;
+                if (following)
+                {
+                    Debug.Log("telling him to stop");
+                    Debug.Log(Vector3.Distance(transform.position, goal.transform.position));
+                    agent.destination = transform.position;
+                    following = false;
+                }
+            }
+
+            // fire while in range
+            if ((goal.position - transform.position).sqrMagnitude <= hitRange * hitRange)
+            {
+                if (firingCoroutine == null)
+                    firingCoroutine = StartCoroutine(FireWhenReady());
+            }
+            else if (firingCoroutine != null)
+            {
+                StopCoroutine(firingCoroutine);
+                firingCoroutine = null;
             }
         }
 
-        // fire while in range
-        if ((goal.position - transform.position).sqrMagnitude <= hitRange * hitRange)
+        private IEnumerator FireWhenReady()
         {
-            if(firingCoroutine == null)
-                firingCoroutine = StartCoroutine(FireWhenReady());
+            while (true)
+            {
+                gun.Fire();
+                yield return new WaitForSeconds(fireDelay);
+            }
         }
-        else if(firingCoroutine != null)
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Throwing-Functionality
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        public void setGrabed()
         {
-            StopCoroutine(firingCoroutine);
-            firingCoroutine = null;
+            velocityEstimator.BeginEstimatingVelocity();
+            //evtl bool grabed? For Movementdeaktivation (grabed=true)
+        }
+
+        public void setFree()
+        {
+            velocityEstimator.FinishEstimatingVelocity();
+            gameObject.GetComponent<Rigidbody>().velocity = velocityEstimator.GetVelocityEstimate();
+            gameObject.GetComponent<Rigidbody>().position = velocityEstimator.transform.position;
+            //(grabed=false)
         }
     }
-
-    private IEnumerator FireWhenReady()
-    {
-        while (true)
-        {
-            gun.Fire();
-            yield return new WaitForSeconds(fireDelay);
-        }
-    }
-
 }
