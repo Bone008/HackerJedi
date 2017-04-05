28e3ded5417f088916e8982b2bf31c035c60ba89
Far Rechner <far-rechner@uni>
2017-04-05 11:55:50 +0200

    added gun controls from controllers

--- Assets/Scripts/HackerPlayer.cs
+++ Assets/Scripts/HackerPlayer.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class HackerPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void OnEnable () {
+        var controllerInputs = GetComponentsInChildren<SteamVR_TrackedController>();
 
-        foreach (SteamVR_RenderModel model in GetComponentsInChildren<SteamVR_RenderModel>())
-            model.gameObject.SetActive(false);
+        foreach(SteamVR_TrackedController controller in controllerInputs)
+            controller.TriggerClicked += Controller_TriggerClicked;
+    }
 
+    private void OnDisable()
+    {
+        var controllerInputs = GetComponentsInChildren<SteamVR_TrackedController>();
+
+        foreach (SteamVR_TrackedController controller in controllerInputs)
+            controller.TriggerClicked -= Controller_TriggerClicked;
+    }
+
+    private void Controller_TriggerClicked(object sender, ClickedEventArgs e)
+    {
+        GameObject controllerObj = ((Component)sender).gameObject;
+
+        Gun gun = controllerObj.GetComponentInChildren<Gun>();
+        if (gun != null)
+            gun.Fire();
     }
 
     // Update is called once per frame
