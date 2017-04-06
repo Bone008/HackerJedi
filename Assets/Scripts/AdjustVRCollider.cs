5e24f9a84629c005c53687bd1fdec16f90dfc422
Peter Greth <peter.greth@gmx.de>
2017-04-06 11:44:11 +0200

    Improved player collider to stick to ground, removed hackerplayer script and collider from HackerRig

---
+++ Assets/Scripts/AdjustVRCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustVRCollider : MonoBehaviour {
+
+    public Transform head;
+    public float overHeadOffset = 0.2f;
+
+    private CapsuleCollider capsCollider;
+
+	void Start () {
+        capsCollider = GetComponent<CapsuleCollider>();
+	}
+	
+	void Update () {
+        // scale collider to stick on the ground and to stay vertical
+        capsCollider.height = head.localPosition.y + overHeadOffset;
+        capsCollider.center = new Vector3(0, - capsCollider.height / 2 + overHeadOffset, 0);
+        capsCollider.transform.rotation = Quaternion.identity;
+	}
+}
