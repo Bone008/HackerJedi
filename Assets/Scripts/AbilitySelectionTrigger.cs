e405e3f331aea5917146f9c9e90be2a3baa9baaa
Far Rechner <far-rechner@uni>
2017-04-06 15:36:02 +0200

    perfected ability selection (almost)

--- Assets/Scripts/AbilitySelectionTrigger.cs
+++ Assets/Scripts/AbilitySelectionTrigger.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class AbilitySelectionTrigger : MonoBehaviour {
 
     public AbilitySelectionWheel wheel;
+    public Material materialRegular;
+    public Material materialHighlight;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("trigger enter", other);
         var element = other.GetComponent<AbilitySelectionElement>() ?? other.GetComponentInParent<AbilitySelectionElement>();
-        if(element != null)
+        if (element != null)
+        {
+            if(wheel.SelectedElement != null)
+            {
+                wheel.SelectedElement.GetComponentInChildren<Renderer>().sharedMaterial = materialRegular;
+            }
+
             wheel.SelectedElement = element;
+            other.GetComponentInChildren<Renderer>().sharedMaterial = materialHighlight;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         var element = other.GetComponent<AbilitySelectionElement>() ?? other.GetComponentInParent<AbilitySelectionElement>();
-        if(wheel.SelectedElement == element)
+        if (wheel.SelectedElement == element)
+        {
             wheel.SelectedElement = null;
+            other.GetComponentInChildren<Renderer>().sharedMaterial = materialRegular;
+        }
     }
 }
--- Assets/Scripts/AbilitySelectionWheel.cs
+++ Assets/Scripts/AbilitySelectionWheel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class AbilitySelectionWheel : MonoBehaviour {
+public class AbilitySelectionWheel : MonoBehaviour
+{
 
-    public Transform selectionPreview;
+    public AbilitySelectionTrigger selectionSphere;
+    public float selectionSphereMultiplier;
     public GameObject[] abilityPrefabs;
+    public float initAnimationDuration;
 
     // this is updated automatically by the selectionPreview by its collider trigger
     public AbilitySelectionElement SelectedElement { get; set; }
@@ -16,14 +20,65 @@ public class AbilitySelectionWheel : MonoBehaviour {
         Debug.Assert(abilityPrefabs != null && abilityPrefabs.Length > 0, "no ability prefabs configured", gameObject);
     }
 
+    void Start()
+    {
+        StartCoroutine(SpawnAnimation());
+    }
+
+
+    private IEnumerator SpawnAnimation()
+    {
+        var elements = GetComponentsInChildren<AbilitySelectionElement>();
+        var animatedElements = elements.Select(e => new
+        {
+            e.transform,
+            collider = e.GetComponentInChildren<SphereCollider>(),
+            initialPosition = e.transform.localPosition
+        }).ToArray();
+
+
+        float t = 0;
+        while (t < initAnimationDuration)
+        {
+            foreach (var e in animatedElements)
+            {
+                Vector3 inverseScale = e.collider.transform.localScale;
+                inverseScale.x = 1 / inverseScale.x;
+                inverseScale.y = 1 / inverseScale.y;
+                inverseScale.z = 1 / inverseScale.z;
+
+                e.transform.localPosition = (t / initAnimationDuration) * e.initialPosition;
+                e.collider.center = Vector3.Scale((1 - (t / initAnimationDuration)) * e.initialPosition, inverseScale);
+            }
+
+            yield return null;
+            t += Time.deltaTime;
+        }
+
+        foreach (var e in animatedElements)
+        {
+            e.transform.localPosition = e.initialPosition;
+            e.collider.center = Vector3.zero;
+        }
+    }
+
+
     public void SetPreviewPosition(Vector2 position)
     {
-        selectionPreview.transform.localPosition = position * 0.1f;
+        selectionSphere.transform.localPosition = position * selectionSphereMultiplier;
     }
 
     // returns the selected AbilityType, or null when the selector is not in contact with any button
     public AbilityType? ConfirmSelection(Vector2 position) // ignored
     {
+        SetPreviewPosition(position);
+
+        // force a collider update
+        var coll = selectionSphere.GetComponent<Collider>();
+        coll.enabled = false;
+        coll.enabled = true;
+
+
         if (SelectedElement == null)
             return null;
         else
