3f04d6c7d957b89628e34d956206a1110f510f38
Peter Greth <peter.greth@gmx.de>
2017-04-06 14:37:29 +0200

    Added layers for hacker / enemy to prevent enemies hitting each other

--- Assets/Scripts/Abilities/Gun.cs
+++ Assets/Scripts/Abilities/Gun.cs
@@ -14,6 +14,9 @@ public class Gun : AbstractAbility {
 
     public override AbilityType Type { get { return abilityType; } }
 
+    [HideInInspector]
+    public int layer = 0;
+
     protected override void OnTriggerDown()
     {
         var shootingDirection = nozzle.transform.TransformDirection(Vector3.forward).normalized;
@@ -25,6 +28,9 @@ public class Gun : AbstractAbility {
 
         // store damage amount of gun in projectile
         projectile.GetComponent<Projectile>().damageAmount = damageAmount;
+
+        // store layer
+        projectile.layer = layer;
     }
 
 }
