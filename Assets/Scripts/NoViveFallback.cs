abcd9e3cd8cd87f7ecd2bf100109ef0ec5072898
Lukas <Bone008@users.noreply.github.com>
2017-04-07 12:04:29 +0200

    automatic no-vr-fallback

--- Assets/Scripts/NoViveFallback.cs
+++ Assets/Scripts/NoViveFallback.cs
@@ -5,25 +5,33 @@ using Valve.VR;
 
 public class NoViveFallback : MonoBehaviour {
 
-    public Transform headCamera;
+    public GameObject nonVRFallback;
+    //public Transform headCamera;
 
 	// Use this for initialization
 	void Awake () {
-        if (SteamVR.instance.hmd_TrackingSystemName.Contains("oculus"))
+        if(!SteamVR.active)
         {
-            // move up so the camera doesn't spawn on the floor
-            transform.localPosition += Vector3.up * 1.0f;
-
-            // attach gun to head instead of controllers
-            var gun = GetComponentInChildren<Gun>();
-            if(gun != null)
-            {
-                gun.transform.SetParent(headCamera, false);
-                // move to a position in view slightly to the right
-                gun.transform.localPosition = new Vector3(0.1f, -0.169f, 0.402f);
-                gun.transform.localEulerAngles = new Vector3(14, 0, 0);
-            }
+            nonVRFallback.SetActive(true);
+            gameObject.SetActive(false);
+            return;
         }
+
+        //if (SteamVR.instance.hmd_TrackingSystemName.Contains("oculus"))
+        //{
+        //    // move up so the camera doesn't spawn on the floor
+        //    transform.localPosition += Vector3.up * 1.0f;
+
+        //    // attach gun to head instead of controllers
+        //    var gun = GetComponentInChildren<Gun>();
+        //    if(gun != null)
+        //    {
+        //        gun.transform.SetParent(headCamera, false);
+        //        // move to a position in view slightly to the right
+        //        gun.transform.localPosition = new Vector3(0.1f, -0.169f, 0.402f);
+        //        gun.transform.localEulerAngles = new Vector3(14, 0, 0);
+        //    }
+        //}
 
     }
 
