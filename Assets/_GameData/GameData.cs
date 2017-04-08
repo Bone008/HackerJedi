de5a0eeaa35d6a3e9daaa07371c46a3566875b1d
Jeff-Owens Iyalekhue <j.oiyalekhue@hotmail.de>
2017-04-08 18:18:48 +0200

    MK_Glow hinzugefügt und LevelGenerator verbessert(evtl. werden die blöcke wieder tiefer generiert... das navmesh sollte nochmal gecheckt werden)

---
+++ Assets/_GameData/GameData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameData
+{
+    private static GameData instance;
+
+    public int randomizeWorldinProgress = 0;
+    public int levels = 3;
+
+    private GameData()
+    {
+        if (instance != null)
+            return;
+        instance = this;
+    }
+
+    public static GameData Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new GameData();
+            return instance;
+        }
+    }
+}
