3e0fb917cc485ee6c87565ec744dd4cd03149bbe
Peter Greth <peter.greth@gmx.de>
2017-04-19 15:35:38 +0200

    Initial import of curved ui again from https://forum.unity3d.com/threads/warp-bend-the-ui.283066/

---
+++ Assets/ImportStuff/CurvedUI/Scripts/MappedGraphicMaterialModifier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MappedGraphicMaterialModifier : MonoBehaviour, IMaterialModifier
+{
+	#region IMaterialModifier
+
+	public Material GetModifiedMaterial(Material material)
+	{
+		CanvasMapping mapping = GetComponentInParent<CanvasMapping>();
+		if (mapping != null)
+		{
+			mapping.SetCanvasToScreenParameters(material);
+		}
+
+		return material;
+	}
+
+	#endregion
+}
