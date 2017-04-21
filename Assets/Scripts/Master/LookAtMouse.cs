d12c2810407dc56473ddcc693c4340a9f2dc02f8
Lukas <Bone008@users.noreply.github.com>
2017-04-21 17:28:45 +0200

    added new Master eye model to scene + made its rotation a bit smoother + slightly improved lighting

--- Assets/Scripts/Master/LookAtMouse.cs
+++ Assets/Scripts/Master/LookAtMouse.cs
@@ -7,6 +7,9 @@ public class LookAtMouse : MonoBehaviour {
     public Camera raycastOrigin;
     public float rotationSpeed = 1.0f;
 
+    private Vector3 lastHitPoint = Vector3.zero;
+    private Vector3 currVelocity = Vector3.zero;
+
 	void Start () {
 		
 	}
@@ -15,20 +18,23 @@ public class LookAtMouse : MonoBehaviour {
         if (!raycastOrigin.enabled)
             return;
 
-        // get aimed-for object via Raycast
-        Transform objectHit = null;
+        // get aimed-for point via Raycast
         RaycastHit hit;
         Ray ray = raycastOrigin.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
-            objectHit = hit.transform;
+            lastHitPoint = hit.point;
         }
 
         // rotate eye to point to raycast hit
-        if (objectHit)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(hit.point - raycastOrigin.transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
+        Quaternion targetRotation = Quaternion.LookRotation(lastHitPoint - raycastOrigin.transform.position);
+        Vector3 targetEuler = targetRotation.eulerAngles;
+        Vector3 euler = transform.eulerAngles;
+        // what we would really want is Quaternion.SmoothDamp, but that does not exist
+        // Vector3.SmoothDamp also kinda works, but is unreliable because it is not made for rotations
+        euler.x = Mathf.SmoothDampAngle(euler.x, targetEuler.x, ref currVelocity.x, 0.15f, rotationSpeed, Time.deltaTime);
+        euler.y = Mathf.SmoothDampAngle(euler.y, targetEuler.y, ref currVelocity.y, 0.15f, rotationSpeed, Time.deltaTime);
+        euler.z = Mathf.SmoothDampAngle(euler.z, targetEuler.z, ref currVelocity.z, 0.15f, rotationSpeed, Time.deltaTime);
+        transform.eulerAngles = euler;
     }
 }
