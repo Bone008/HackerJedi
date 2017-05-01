2fe4837bbe16f93dba718a76b3ce036d9c6ba815
Moritz <moritz.rocksien@tum.de>
2017-05-01 07:45:21 +0200

    Tried to fix some of the bugs for the enemies, probably created 100 more. i hate these annoying little shitheads...

--- Assets/Scripts/Enemies/RangedGruntEnemy.cs
+++ Assets/Scripts/Enemies/RangedGruntEnemy.cs
@@ -5,14 +5,18 @@ using UnityEngine.AI;
 
 public class RangedGruntEnemy : EnemyBase
 {
-    public float newTargetPosThreshhold = 1;
-    public float hitRange, stoppingDistance;
+    public float newTargetPosThreshhold = 1f;
+    public float rotationSpeed = 3.5f;
+    public float hitRange, stoppingDistance, floorTime;
 
     public GameObject explo;
 
     private Transform goal;
     private Vector3 oldPos;
     private NavMeshAgent agent;
+    private Rigidbody rb;
+    private bool isOnFloor;
+    private float startTimeFloorContact = 0;
 
     void Start()
     {
@@ -25,9 +29,13 @@ public class RangedGruntEnemy : EnemyBase
             return;
         }
 
+        rb = GetComponent<Rigidbody>();
+
+        isOnFloor = false;
         goal = player.transform;
         oldPos = goal.position;
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = true;
 
         if (agent.isOnNavMesh)
             agent.destination = goal.position;              
@@ -35,36 +43,30 @@ public class RangedGruntEnemy : EnemyBase
 
     void Update()
     {
-        if (agent.isOnNavMesh)
+        
+        if (agent.enabled)
         {
-            if (Vector3.Distance(transform.position, goal.position) < stoppingDistance /*&& !agent.isStopped*/)
+            //Handle enemy in functional state and on navmesh
+            float dist = Vector3.Distance(transform.position, goal.position);
+            if (agent.isOnNavMesh)
             {
-                agent.isStopped = true;
-                //agent.destination = transform.position;
-                agent.enabled = false;
-            }
-
-            if (Vector3.Distance(transform.position, goal.position) > hitRange /*&& agent.isStopped*/)
-            {                
-                agent.enabled = true;
-                //agent.destination = goal.position;
-                agent.isStopped = false;
-            }
-
-            if (Vector3.Distance(oldPos, goal.position) > newTargetPosThreshhold /*&& !agent.isStopped*/)
-            {
-                agent.destination = goal.position;
-                oldPos = goal.position;
-            }
-
-            if (agent.velocity.x >= -0.05f && agent.velocity.x <= 0.05f
-                && agent.velocity.z >= -0.1f && agent.velocity.z <= 0.1f)
-            {
-                transform.LookAt(goal);
+                handleFunctionalEnemy(dist);
             }
         }
-        //Debug.Log(agent.isStopped);
-        //Debug.Log(Vector3.Distance(transform.position, goal.position));        
+        else
+        {
+            //Handle enemy in non-functional state and/or off navmesh
+            handleDisabledEnemy();
+        }        
+    }
+    
+    private void rotateTowards(Transform target)
+    {
+        Vector3 targetDir = target.position - transform.position;
+        targetDir = new Vector3(targetDir.x, 0, targetDir.z);
+        float step = rotationSpeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);        
+        transform.rotation = Quaternion.LookRotation(newDir);        
     }
 
     public void OnDeath()
@@ -72,5 +74,83 @@ public class RangedGruntEnemy : EnemyBase
         Instantiate(explo, transform.position, transform.rotation);
         Destroy(gameObject);
     }
-    
+
+    private void handleFunctionalEnemy(float dist)
+    {
+        //Stop enemy if close enough
+        if (dist < stoppingDistance && !agent.isStopped)
+        {
+            agent.isStopped = true;
+            //agent.destination = transform.position;
+            //agent.enabled = false;                
+        }
+
+        //follow player if distance too big
+        if (dist > hitRange && agent.isStopped)
+        {
+            //agent.enabled = true;
+            //agent.destination = goal.position;
+            agent.isStopped = false;
+            agent.destination = goal.position;
+            oldPos = goal.position;
+        }
+
+        //Only update goal position if platform moved reasonable amount
+        if (Vector3.Distance(goal.position, oldPos) > newTargetPosThreshhold && !agent.isStopped)
+        {
+            agent.destination = goal.position;
+            oldPos = goal.position;
+        }        
+
+        //Always look at player when in hit range
+        if (dist < hitRange)
+        {
+            agent.updateRotation = false;
+            //transform.LookAt(new Vector3(goal.position.x, transform.position.y, goal.position.z));
+            rotateTowards(goal);
+        }
+        else
+        {
+            agent.updateRotation = true;
+        }
+        //Debug.Log("onNavMesh loop " + Time.time + " " + agent.isOnNavMesh);
+    }
+
+
+    private void handleDisabledEnemy()
+    {
+        if (rb.velocity.sqrMagnitude < .1f)
+        {
+            if (isOnFloor && Time.time - startTimeFloorContact > floorTime)
+            {
+                //implement get up animation
+                //for now:
+                agent.enabled = true;
+                agent.isStopped = false;
+                agent.destination = goal.position;
+                oldPos = goal.position;
+            }
+            //add handling of disabled enemy outside of standard grid (move towards grid and lift up...)
+        }
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RoomBlock"))
+        {
+            isOnFloor = true;
+            startTimeFloorContact = Time.time;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RoomBlock"))
+        {
+            isOnFloor = false;
+        }
+    }
+
 }
