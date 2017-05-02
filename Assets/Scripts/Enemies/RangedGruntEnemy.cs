ad91d7c90345bfaa2b848c65d27c591171df89b7
Moritz <moritz.rocksien@tum.de>
2017-05-03 00:49:11 +0200

    hopefully fixed enemies, please report bugs

--- Assets/Scripts/Enemies/RangedGruntEnemy.cs
+++ Assets/Scripts/Enemies/RangedGruntEnemy.cs
@@ -6,8 +6,10 @@ using UnityEngine.AI;
 public class RangedGruntEnemy : EnemyBase
 {
     public float newTargetPosThreshhold = 1f;
-    public float rotationSpeed = 3.5f;
-    public float hitRange, stoppingDistance, floorTime;
+    public float rotationSpeed = 5f;
+    public Vector3 offset = new Vector3(.5f, 1.533333f, .5f);
+    public float hitRange, stoppingDistance, floorTime, recoveryMovementSpeed;
+    public Vector3 bounds;
 
     public GameObject explo;
 
@@ -15,8 +17,9 @@ public class RangedGruntEnemy : EnemyBase
     private Vector3 oldPos;
     private NavMeshAgent agent;
     private Rigidbody rb;
-    private bool isOnFloor;
-    private float startTimeFloorContact = 0;
+    private bool recovering, timerRunning;
+    private float startTimeResting = 0;
+    private int collisions = 0;
 
     void Start()
     {
@@ -30,8 +33,9 @@ public class RangedGruntEnemy : EnemyBase
         }
 
         rb = GetComponent<Rigidbody>();
-
-        isOnFloor = false;
+       
+        recovering = false;
+        timerRunning = false;
         goal = player.transform;
         oldPos = goal.position;
         agent = GetComponent<NavMeshAgent>();
@@ -119,38 +123,127 @@ public class RangedGruntEnemy : EnemyBase
 
     private void handleDisabledEnemy()
     {
-        if (rb.velocity.sqrMagnitude < .1f)
-        {
-            if (isOnFloor && Time.time - startTimeFloorContact > floorTime)
+        if (!recovering)
+        {           
+            if (rb.velocity.sqrMagnitude < .1f)
             {
-                //implement get up animation
-                //for now:
-                agent.enabled = true;
-                agent.isStopped = false;
-                agent.destination = goal.position;
-                oldPos = goal.position;
+                Debug.Log("test2 " + collisions);
+                if (!timerRunning && collisions > 0)
+                {
+                    startTimeResting = Time.time;
+                    timerRunning = true;
+                }
+                if (timerRunning && collisions == 0)
+                {                
+                    timerRunning = false;
+                }
+
+                if (collisions > 0 && Time.time - startTimeResting > floorTime)
+                {
+                    recovering = true;
+                    timerRunning = false;
+                    moveToValidNavPos();
+                    //implement get up animation
+                    //for now:
+                    //agent.enabled = true;
+                    //agent.isStopped = false;
+                    //agent.destination = goal.position;
+                    //oldPos = goal.position;
+                }
+                //add handling of disabled enemy outside of standard grid (move towards grid and lift up...)
+                //moveToValidNavPos();
+            } else
+            {
+                timerRunning = false;
             }
-            //add handling of disabled enemy outside of standard grid (move towards grid and lift up...)
         }
+        else
+        {
+            //is there even any "else"?
+        }
+    }
+
+    private void moveToValidNavPos()
+    {
+        float x = transform.position.x;       
+        float z = transform.position.z;
+
+        if (x > -bounds.x && x < bounds.x && z > -bounds.z && z < bounds.z)
+        {
+            //Inside playing field. what happens for rail?
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(x, offset.y, z)));            
+        }
+
+        else if (x > bounds.x && z > -bounds.z && z < bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(bounds.x - offset.x, offset.y, z)));        
+
+        else if (x < -bounds.x && z > -bounds.z && z < bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(-bounds.x + offset.x, offset.y, z)));        
+
+        else if (x > -bounds.x && x < bounds.x && z > bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(x, offset.y, bounds.z - offset.z)));
+
+        else if (x > -bounds.x && x < bounds.x && z < -bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(x, offset.y, -bounds.z + offset.z)));
+
+        else if (x > bounds.x && z > bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(bounds.x - offset.x, offset.y, bounds.z - offset.z)));
+
+        else if (x < -bounds.x && z > bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(-bounds.x + offset.x, offset.y, bounds.z - offset.z)));
+
+        else if (x > bounds.x && z < -bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(bounds.x - offset.x, offset.y, -bounds.z + offset.z)));
+
+        else if (x < -bounds.x && z < -bounds.z)
+            StartCoroutine(moveToTargetPosAndTurnUp(new Vector3(-bounds.x + offset.x, offset.y, -bounds.z + offset.z)));
+
+
     }
 
 
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("RoomBlock"))
-        {
-            isOnFloor = true;
-            startTimeFloorContact = Time.time;
-        }
+        collisions++;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("RoomBlock"))
+        collisions--;        
+    }
+
+    private IEnumerator moveToTargetPosAndTurnUp(Vector3 targetPos)
+    {
+        Transform initTrans = this.transform;
+        float initDist = Vector3.Distance(initTrans.position, targetPos);
+        Quaternion goalRot = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        Debug.Log("entered coroutine " + Time.time);
+        rb.isKinematic = true;
+
+        while(targetPos != transform.position || transform.rotation != goalRot)
         {
-            isOnFloor = false;
+            Debug.Log(Time.time);
+            //move towards position
+            float step = recoveryMovementSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+
+            //rotate upwards
+            //Quaternion rot = Quaternion.Slerp(initTrans.rotation, goalRot, Vector3.Distance(transform.position, targetPos)/initDist);
+            step = rotationSpeed * 10 * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRot, step);
+            //transform.rotation = Quaternion.Lerp(initTrans.rotation, goalRot, 1 - (Vector3.Distance(transform.position, targetPos) / initDist));
+
+            yield return null;
         }
+
+        Debug.Log("exited coroutine loop " + Time.time);
+        recovering = false;
+        agent.enabled = true;
+        agent.isStopped = false;
+        agent.destination = goal.position;
+        oldPos = goal.position;
+        rb.isKinematic = false;
     }
 
 }
