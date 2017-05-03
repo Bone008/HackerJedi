c39c212b6b84e277f088df43906369c6d014b7d7
Moritz <moritz.rocksien@tum.de>
2017-05-04 00:12:24 +0200

    enemies rolling around on the floor or soaring through the air wont shoot at the player any longer

--- Assets/Scripts/Enemies/RangedGruntEnemy.cs
+++ Assets/Scripts/Enemies/RangedGruntEnemy.cs
@@ -20,6 +20,7 @@ public class RangedGruntEnemy : EnemyBase
     private bool recovering, timerRunning;
     private float startTimeResting = 0;
     private int collisions = 0;
+    private ShootPlayer shootPlayer;
 
     void Start()
     {
@@ -41,6 +42,8 @@ public class RangedGruntEnemy : EnemyBase
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = true;
 
+        shootPlayer = GetComponent<ShootPlayer>();
+
         if (agent.isOnNavMesh)
             agent.destination = goal.position;              
     }
@@ -50,6 +53,9 @@ public class RangedGruntEnemy : EnemyBase
         
         if (agent.enabled)
         {
+            if (!shootPlayer.enabled)
+                shootPlayer.enabled = true;
+
             //Handle enemy in functional state and on navmesh
             float dist = Vector3.Distance(transform.position, goal.position);
             if (agent.isOnNavMesh)
@@ -59,6 +65,9 @@ public class RangedGruntEnemy : EnemyBase
         }
         else
         {
+            if (shootPlayer.enabled)
+                shootPlayer.enabled = false;
+
             //Handle enemy in non-functional state and/or off navmesh
             handleDisabledEnemy();
         }        
@@ -86,15 +95,11 @@ public class RangedGruntEnemy : EnemyBase
         if (dist < stoppingDistance && !agent.isStopped)
         {
             agent.isStopped = true;
-            //agent.destination = transform.position;
-            //agent.enabled = false;                
         }
 
         //follow player if distance too big
         if (dist > hitRange && agent.isStopped)
         {
-            //agent.enabled = true;
-            //agent.destination = goal.position;
             agent.isStopped = false;
             agent.destination = goal.position;
             oldPos = goal.position;
@@ -111,14 +116,12 @@ public class RangedGruntEnemy : EnemyBase
         if (dist < hitRange)
         {
             agent.updateRotation = false;
-            //transform.LookAt(new Vector3(goal.position.x, transform.position.y, goal.position.z));
             rotateTowards(goal);
         }
         else
         {
             agent.updateRotation = true;
-        }
-        //Debug.Log("onNavMesh loop " + Time.time + " " + agent.isOnNavMesh);
+        }        
     }
 
 
@@ -143,23 +146,12 @@ public class RangedGruntEnemy : EnemyBase
                     recovering = true;
                     timerRunning = false;
                     moveToValidNavPos();
-                    //implement get up animation
-                    //for now:
-                    //agent.enabled = true;
-                    //agent.isStopped = false;
-                    //agent.destination = goal.position;
-                    //oldPos = goal.position;
                 }
-                //add handling of disabled enemy outside of standard grid (move towards grid and lift up...)
-                //moveToValidNavPos();
+
             } else
             {
                 timerRunning = false;
             }
-        }
-        else
-        {
-            //is there even any "else"?
         }
     }
 
@@ -215,8 +207,6 @@ public class RangedGruntEnemy : EnemyBase
 
     private IEnumerator moveToTargetPosAndTurnUp(Vector3 targetPos)
     {
-        Transform initTrans = this.transform;
-        float initDist = Vector3.Distance(initTrans.position, targetPos);
         Quaternion goalRot = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         rb.isKinematic = true;
 
@@ -227,10 +217,8 @@ public class RangedGruntEnemy : EnemyBase
             transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
 
             //rotate upwards
-            //Quaternion rot = Quaternion.Slerp(initTrans.rotation, goalRot, Vector3.Distance(transform.position, targetPos)/initDist);
             step = rotationSpeed * 10 * Time.deltaTime;
             transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRot, step);
-            //transform.rotation = Quaternion.Lerp(initTrans.rotation, goalRot, 1 - (Vector3.Distance(transform.position, targetPos) / initDist));
 
             yield return null;
         }
