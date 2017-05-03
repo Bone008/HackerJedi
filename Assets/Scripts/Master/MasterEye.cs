e63de195948e70d76075bb9a1d0c7270e050f892
Peter Greth <peter.greth@gmx.de>
2017-05-03 15:40:17 +0200

    Integrated signal lost video into master ondeath

--- Assets/Scripts/Master/MasterEye.cs
+++ Assets/Scripts/Master/MasterEye.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using MKGlowSystem;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MasterEye : MonoBehaviour {
 
+    public float signalLostDuration = 5.0f;
     public GameObject deathExplosion;
-    public RectTransform healthBarPanel;
-    
+    public LayerMask explosionLayer;
+
+    public VideoPlayer signalLostPlayer;
+    public GameObject signalLostBackground;
+
+    public Camera masterCamera;
+    private MKGlow mkGlow;
+
+    private void Start()
+    {
+        mkGlow = masterCamera.GetComponent<MKGlow>();
+    }
+
     public void OnDeath(HealthResource health)
     {
-        // TODO
-        Debug.Log("master deadeded");
-        health.RestoreFullHealth();
-        Instantiate(deathExplosion, transform.position, Quaternion.identity);
+        // explosion only visible to hacker
+        GameObject explosion = Instantiate(deathExplosion, transform.position, Quaternion.identity);
+        explosion.layer = explosionLayer.value;
+
+        // start video, enable black background, disable glow, disable health script (-> only 1 ondeath)
+        signalLostPlayer.Play();
+        signalLostBackground.SetActive(true);
+        mkGlow.enabled = false;
+        health.enabled = false;
+
+        this.Delayed(signalLostDuration, () => 
+        {
+            // stop video, disable background, enable glow and health again
+            signalLostPlayer.Stop();
+            signalLostBackground.SetActive(false);
+            mkGlow.enabled = true;
+            health.enabled = true;
+
+            health.RestoreFullHealth();
+        });
     }
     
 }
