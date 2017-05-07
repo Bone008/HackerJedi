ebac6b5b3c708cd32f7d94e913dba3d6c6f27d70
Lukas <Bone008@users.noreply.github.com>
2017-05-07 11:55:18 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Bone008/HackerJedi""
    This reverts commit d6df5d20eb1d531365af269be93de5371f8fe5ad.

---
+++ Assets/Scripts/Enemies/KillAudioObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAudioObject : MonoBehaviour {
+    AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+        sound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!sound.isPlaying)
+            Destroy(this.gameObject);
+	}
+}
