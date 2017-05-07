cc1ab414670542e181be627392ebfbe42293fce1
Daniel Jazz Young <daniel-young@web.de>
2017-05-07 12:46:44 +0200

    Now you hear music differently depending on location: game <> inbetween

---
+++ Assets/Scripts/AudioMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioLowPassFilter))]
+
+public class AudioMixer : MonoBehaviour {
+    
+	
+	// Update is called once per frame
+	void Update () {
+    
+        
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 2)
+        {
+            GetComponent<AudioLowPassFilter>().cutoffFrequency = 666f;
+        }
+        if (level == 1)
+        {
+            GetComponent<AudioLowPassFilter>().cutoffFrequency = 5000f;
+        }
+    }
+}
