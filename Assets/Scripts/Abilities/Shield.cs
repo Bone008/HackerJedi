24b8eb8b6d4ed371ecc85ac5984dcda32bfa6bc3
Daniel Jazz Young <daniel-young@web.de>
2017-05-08 17:48:25 +0200

    testing shield, need to pull :)

---
+++ Assets/Scripts/Abilities/Shield.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : AbstractAbility
+{
+
+    public float energy;
+    public float drainRate;
+    public float cooldown;
+    private bool isActive;
+    private bool isRecovering;
+
+
+    private void Start()
+    {
+        isActive = false;
+        isRecovering = false;
+    }
+
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            energy -= drainRate * Time.deltaTime;
+        }
+
+    }
+
+    protected override void OnTriggerDown()
+    {
+        base.OnTriggerDown();
+
+    }
+}
