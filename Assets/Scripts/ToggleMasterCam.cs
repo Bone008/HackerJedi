8433649d55059c1281db0ad3c95a994486ae58e8
Lukas <lukas.bonauer@yahoo.de>
2017-05-08 14:41:23 +0200

    added ability to toggle master camera off (F8) even in VR mode to allow switching which perspective is displayed

---
+++ Assets/Scripts/ToggleMasterCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ToggleMasterCam : MonoBehaviour {
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Toggle Master Camera"))
+            cam.enabled = !cam.enabled;
+    }
+
+}
