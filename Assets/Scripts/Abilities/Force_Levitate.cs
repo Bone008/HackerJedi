be5e8f9381893a600e6dccf7d3370326e949872d
jungo <ga45jas@mytum.de>
2017-05-09 14:46:11 +0200

    Levitate_Reset ausgelagert Auskommentierte Bereiche entfernt

--- Assets/Scripts/Abilities/Force_Levitate.cs
+++ Assets/Scripts/Abilities/Force_Levitate.cs
@@ -49,13 +49,8 @@ public class Force_Levitate : AbstractUltimate
         }
         else
         {
-            //@HackerPlayer: Ultimate wurde abgebrochen. Ein normaler Move soll ausgeführt werden
             Debug.Log("Ultimate-Tracking not started! #For_Lev OnGripDown()");
-            //Eventuell nun nichtmehr nötig
         }
-        //TODO:
-        //--> set UltimateActive
-        //--> unselect selected Enemies
     }
     protected override void OnGripUp() {
         if (!activated)
@@ -83,22 +78,13 @@ public class Force_Levitate : AbstractUltimate
                     behaviours[counter] = obj;
                 }
             }
-            ActivateEffect<Force_Levitate>(10).StartCoroutine(Reset(behaviours));
+            ActivateEffect<Levitate_Reset>(10).init(behaviours);
 
             DisableUlti();
         }
         else
         {
             Debug.Log("Ultimate-Tracking failed! #For_Lev OnGripUp()");
-        }
-    }
-    IEnumerator Reset(Throwable_OBJ[] levitated)
-    {
-        yield return new WaitForSeconds(9);
-        foreach (Throwable_OBJ col in levitated)
-        {
-            if(col != null)
-                col.setFree();
         }
     }
 
@@ -134,13 +120,8 @@ public class Force_Levitate : AbstractUltimate
         }
         else
         {
-            //@HackerPlayer: Ultimate wurde abgebrochen. Ein normaler Move soll ausgeführt werden
             Debug.Log("Ultimate-Tracking failed! #For_Gun OnTriggerDown()");
-            //Kann evtl entfernt werden
         }
-        //TODO:
-        //--> set UltimateActive
-        //--> unselect selected Enemies
     }
     protected override void OnTriggerUp()
     {
@@ -167,7 +148,7 @@ public class Force_Levitate : AbstractUltimate
 
     private void force()
     {
-        //Gegner in Reichweite (5) werden erkannt
+        //Gegner in Reichweite werden erkannt
         Collider[] cols = Physics.OverlapSphere(transform.position, rangeG);//Kann noch abhängig von Handposition gemacht werden
         foreach (Collider col in cols)
         {
@@ -197,76 +178,3 @@ public class Force_Levitate : AbstractUltimate
         }
     }
 }
-    /*
-    
-    private void searchGestures()
-    {
-        //Für Gd Move muss dauerhaft überprüft werden, wo die Controller sind... Nicht besonders performant. Da sollten wir uns was anderes überlegen.
-        
-        //In OnTriggerUp//Wenn an beiden Seiten Force ausgewählt - Mit ultimateEnabled()
-        //Dann:
-            //Check auf Geste
-                //wenn UltimateActive && (controler1Anfang.y - controller1Ende.y)>0.5 && (controler2Anfang.y - controller2Ende.y)>0.5
-                //--> Gandalf Move Ausführen force(position, Vector3.zero);
-                //Vorschau muss anders gehandled werden
-
-                //wenn (controler1Anfang.y - controller1Ende.y)<0.5 && (controler2Anfang.y - controller2Ende.y)<0.5
-                //--> Force-Levitate allen Gegnern constantforce 10 auf y geben
-
-                //wenn (controller1 und 2 höher als Kopf und nah am Kopf)
-            //Zugriff wie bei Forcepush
-            //return false dass normal fortgesetzt wird
-        //Sonst:
-            //return true dass normal fortgesetzt wird
-        
-    }
-}*/
-//Noch direkt in Forcepush einbauen?
-/*
- In Hackerplayer:
- * wenn UltimateActive() und beide Controller getriggert sind
- * -> Setze Ultimate auf active
- * wenn Ultimate active 
- * -> bei loslassen der beiden Trigger: Check ob Geste passt
- * (was ist falls nur einer losgelassen wird?)
- * wenn Geste passt
- * -> Move und retetUltimateMode
- * 
- * protected bool bothTriggersDown;
-    protected bool bothGripsDown;
-
-    //Should only be called when both triggers are pressed (or one released) and the controllers are at the correct position (-> CheckForStartpoint())
-    public bool SetTriggersDown(bool value, Transform handLeft, Transform handRight)
-    {
-        bool wasDown = bothTriggersDown;
-        //bothTriggersDown = value;
-
-        if (value && !wasDown)
-        {
-            if (CheckForStartpoint(handLeft, handRight))
-            {
-                OnTriggersDown(handLeft.position, handRight.position);
-                bothTriggersDown = value;
-                return true;
-            }
-        }
-        else if (!value && wasDown)
-        {
-            OnTriggerUp(handLeft.position, handRight.position);//Hier muss eventuell noch gecheckt werden, ob der Move auch correct war         !!!!!
-            bothTriggersDown = value;
-            return true;
-        }
-        return false;
-    }
- */
-/*Ablauf:
- * 1. HackerPlayer registriert Drücken von Trigger/Grip                     (Input -> HackerPlayer)
- * 2. HackerPlayer ermittelt, ob Ultimate aufgeladen und möglich ist        (HackerPlayer)
- * 3.1 Falls ja: HackerPlayer fragt HandPos ab und schickt es an Ultimate   (Ultimate: SetTriggers/Grips)
- * 3.2 Falls Nein: HackerPlayer setzt normalen Move um                      (Ability)
- * 4. Ultimate Checkt, ob Position und Rotation stimmen                     (Ultimate: SetTrigger/Grips -> CheckPosRot)
- * 5.1 Stimmt: Return true und ggf OnTriggerDown                            (SetTrigger ~> HackerPlayer)
- * 5.2 Nicht: Return false, Hackerplayer bricht Ultimate ab                 (SetTrigger -> HackerPlayer)
- * 6. On...Down speichert Positionen                                        (On...Down)
- * 7. Bei Loslassen eines Buttons: Check auf Ultimateabbruch                (
- */
