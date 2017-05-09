e7651b57d52c8b1e7fad020032bd1640be87dd3b
Daniel Jazz Young <daniel-young@web.de>
2017-05-09 14:41:08 +0200

    Changed volume.

---
+++ Assets/Scripts/Abilities/ShieldPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class ShieldPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
